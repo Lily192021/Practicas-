da6bf2e81e76bc2fefd460dec9ea41285517e5c7
Lily192021 <80777955+Lily192021@users.noreply.github.com>
2022-02-27 15:01:35 -0600

    Add files via upload

---
+++ PRACTICAS/Semana 3 practica/vue.js
@@ -0,0 +1,8 @@
+Vue.component("example",{
+    data:function(){
+        return{
+            counter:0
+        },
+        template: "<button>button v-on:click="counter++Click here </button>   
+  
+    
